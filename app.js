119a9a31f63aa5d5a5fb8e58366dfc6a444f7e4f
TANNER MECK <steve.meck246@gmail.com>
2021-06-30 14:56:53 -0700

    reset button added

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const numberInput = document.getElementById('number-input');
 const numberButton = document.getElementById('number-button');
 const triesRemaining = document.getElementById('tries-remaining');
 const hints = document.getElementById('hints');
+const reset = document.getElementById('reset');
 // set event listeners 
   // get user input
   // use user input to update state 
@@ -17,8 +18,10 @@ numberButton.addEventListener('click', () => {
     attempts--;
     if (attempts === 0){
         numberButton.disabled = true;
+        reset.visible = true;
     }
     triesRemaining.textContent = `${attempts} tries reamining`;
     const check = checkNumber(numberInput.value, number);
     hints.textContent = check;
+    
 });
